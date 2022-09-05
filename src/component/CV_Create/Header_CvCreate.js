c801df63124871194c749f241b882e14af7ee66d
Phong1412-1412 <phongbuibsp1@gmail.com>
2022-09-05 13:59:19 +0700

    122

--- src/component/CV_Create/Header_CvCreate.js
+++ src/component/CV_Create/Header_CvCreate.js
@@ -2,57 +2,84 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../Redux/Action/UserAction";
-<<<<<<< HEAD
-import { FaGift, FaLongArrowAltUp,FaUser,FaCog,FaSignOutAlt } from "react-icons/fa";
-import {Link} from 'react-router-dom'
-=======
 import { FaGift, FaLongArrowAltUp, FaEye, FaElementor, FaRegEnvelope, FaSuitcaseRolling, FaDoorOpen, FaExchangeAlt} from "react-icons/fa";
 
->>>>>>> 43c334bdeef2c7ee334f4bf6267428bbad06abcb
 import './style.css'
 
-function Header_CvCreate() {
+function Header_CvCreate () {
     const dispatch = useDispatch();
 
     const userLogin = useSelector((state) => state.userLogin);
-<<<<<<< HEAD
-    const { userInfo, loading, error } = userLogin;
-=======
     const {userInfo, loading, error} = userLogin;
-    const [login, setLogin] = useState(false);
->>>>>>> 43c334bdeef2c7ee334f4bf6267428bbad06abcb
-
-    
     return (
         <div class="wrappar">
-            <header>
-                <div class="noti">
-                    <p>Tài khoản của bạn chưa được xác thực. Vui lòng xác thực tài khoản</p>
-                    <a href="#">tại đây</a>
+        <header>
+           <div class="noti">
+                <p>Tài khoản của bạn chưa được xác thực. Vui lòng xác thực tài khoản</p>
+                <a href="#">tại đây</a>
+           </div>
+           <nav class="header_nav">
+                <div >
+                    <ul class="nav_links">
+                        <li><a href="#"><img src="images/logo.png" alt="logo"></img></a></li>
+                        <li>
+                            <a href="/">Việc làm</a>
+                            <div class="subnav">
+                                <div>
+                                    <ul class="subnav_list">
+                                        <li>Tìm việc làm</li>
+                                        <li>Việc làm đã ứng tuyển</li>
+                                        <li>Việc làm đã lưu</li>
+                                        <li>Việc làm phủ hợp</li>
+                                        <li>Việc làm từ xa</li>
+                                        <li>Việc là IT</li>
+                                        <li>Việc làm senior</li>
+                                    </ul>
+                                </div>
+                                <div class="subnav_img">
+                                    <img src="https://www.topcv.vn/v4/image/nav-job.png?v=1.0.0" alt=""></img>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/cv_create">Hồ sơ & CV</a>
+                            <div class="subnav">
+                                <div>
+                                    <ul class="subnav_list">
+                                        <li>Quản lý CV</li>
+                                        <li>Quản lý cover letter</li>
+                                        <li>Mẫu CV</li>
+                                        <li>Mẫu CV Letter</li>
+                                        <li>Dịch vụ tư vấn CV</li>
+                                        <li>Hướng dẫn viết CV theo ngành nghề</li>
+                                        <li>Top CV profile</li>
+                                    </ul>
+        
+                                </div>
+                                <div class="subnav_img">
+                                    <img src="https://www.topcv.vn/v4/image/nav-cv.png?v=1.0.0" alt=""></img>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Công ty</a>
+                            <div class="subnav">
+                                <div>
+                                    <ul class="subnav_list">
+                                        <li>Danh sách công ty</li>
+                                        <li>Top công ty</li>   
+                                    </ul>
+        
+                                </div>
+                                <div class="subnav_img">
+                                   
+                                </div>
+                            </div>
+                        </li>
+                        <li><a href="#">Phát triển sự nghiệp</a></li>
+                        <li><a href="#">Công cụ</a></li>
+                    </ul>
                 </div>
-<<<<<<< HEAD
-                <nav class="header_nav">
-                    <div >
-                        <ul class="nav_links">
-                            <li><a href="#"><img src="images/logo.png" alt="logo"></img></a></li>
-                            <li>
-                                <a href="/">Việc làm</a>
-                                <div class="subnav">
-                                    <div>
-                                        <ul class="subnav_list">
-                                            <li>Tìm việc làm</li>
-                                            <li>Việc làm đã ứng tuyển</li>
-                                            <li>Việc làm đã lưu</li>
-                                            <li>Việc làm phủ hợp</li>
-                                            <li>Việc làm từ xa</li>
-                                            <li>Việc là IT</li>
-                                            <li>Việc làm senior</li>
-                                        </ul>
-                                    </div>
-                                    <div class="subnav_img">
-                                        <img src="https://www.topcv.vn/v4/image/nav-job.png?v=1.0.0" alt=""></img>
-                                    </div>
-=======
                 <div >
                     <ul class="nav_links">
                         <li><a href="#"><img src="images/message.png" alt="logo"></img></a></li>
@@ -99,8 +126,8 @@ function Header_CvCreate() {
                                                            {
                                                             userInfo && (
                                                                 'Đăng Xuất'
-                                                            )|| 'Đăng Nhập'
-                                                           }
+                                                            ) || 'Đăng Nhập'
+                                                           } 
                                                     </p>
                                                 </a>
                                             </li>
@@ -108,111 +135,17 @@ function Header_CvCreate() {
                                       
                                     </ul>
         
->>>>>>> 43c334bdeef2c7ee334f4bf6267428bbad06abcb
                                 </div>
-                            </li>
-                            <li>
-                                <a href="/cv_create">Hồ sơ & CV</a>
-                                <div class="subnav">
-                                    <div>
-                                        <ul class="subnav_list">
-                                            <li>Quản lý CV</li>
-                                            <li>Quản lý cover letter</li>
-                                            <li>Mẫu CV</li>
-                                            <li>Mẫu CV Letter</li>
-                                            <li>Dịch vụ tư vấn CV</li>
-                                            <li>Hướng dẫn viết CV theo ngành nghề</li>
-                                            <li>Top CV profile</li>
-                                        </ul>
-
-                                    </div>
-                                    <div class="subnav_img">
-                                        <img src="https://www.topcv.vn/v4/image/nav-cv.png?v=1.0.0" alt=""></img>
-                                    </div>
+                                <div class="subnav_img">
+                                   
                                 </div>
-                            </li>
-                            <li>
-                                <a href="#">Công ty</a>
-                                <div class="subnav">
-                                    <div>
-                                        <ul class="subnav_list">
-                                            <li>Danh sách công ty</li>
-                                            <li>Top công ty</li>
-                                        </ul>
-
-                                    </div>
-                                    <div class="subnav_img">
-
-                                    </div>
-                                </div>
-                            </li>
-                            <li><a href="#">Phát triển sự nghiệp</a></li>
-                            <li><a href="#">Công cụ</a></li>
-                        </ul>
-                    </div>
-                    <div >
-                        <ul class="nav_links">
-                            <li><a href="#"><img src="images/message.png" alt="logo"></img></a></li>
-                            <li class="nav_links_image name_user">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp_q8yprYUCumkTMHohLWFK6pXeIMCggV-aYyKrr4gjA" alt="logo"></img>
-                                <p>{userInfo?.name}</p>
-                                <div class="subnav">
-                                    <div>
-                                        <ul class="subnav_list_avatar">
-                                            <div>
-                                                <li class="items_icons_text">
-                                                    <FaLongArrowAltUp display={'block'}></FaLongArrowAltUp>
-                                                    <p>Nâng cấp tài khoản vip</p>
-                                                </li>
-                                                <Link to="/profile">
-                                                    <li class="items_icons_text">
-                                                        <FaUser display={'block'}></FaUser>
-                                                        <p>Hồ sơ</p>
-                                                    </li>
-                                                </Link>
-                                                <Link to="/profile/setting">
-                                                    <li class="items_icons_text">
-                                                        <FaCog display={'block'}></FaCog>
-                                                        <p>Chỉnh sửa hồ sơ</p>
-                                                    </li>
-                                                </Link>
-
-                                                <li class="items_icons_text">
-                                                    <FaGift display={'block'}></FaGift>
-                                                    <p>Cài đặt gợi ý việc làm</p>
-                                                </li>
-                                                <li class="items_icons_text">
-                                                    <FaGift display={'block'}></FaGift>
-                                                    <p>Cài đặt nhận Email</p>
-                                                </li>
-                                                <li class="items_icons_text">
-                                                    <FaGift display={'block'}></FaGift>
-                                                    <p>Cài đặt bảo mật</p>
-                                                </li>
-                                                <li class="items_icons_text">
-                                                    <FaGift display={'block'}></FaGift>
-                                                    <p>Đổi mật khẩu</p>
-                                                </li>
-                                                <li class="items_icons_text">
-                                                    <FaSignOutAlt display={'block'}></FaSignOutAlt>
-                                                
-                                                    <a onClick={() => dispatch(logout())} href="#"><p style={{ color: 'red' }}>Đăng xuất</p></a>
-                                                </li>
-                                            </div>
-
-                                        </ul>
-
-                                    </div>
-                                    <div class="subnav_img">
-
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
-        </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+           </nav>
+        </header>
+    </div>
     )
 }
 
