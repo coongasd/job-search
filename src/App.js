ad57f01095ce89cf21a090e5168c701d4473f908
Nhat <nhatgod90888@gmail.com>
2022-09-11 20:14:08 +0700

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MainPage from './component/MainPage/MainPage';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Profile from './component/CV_Profile/Profile'
-import {Routes, Route, Link, NavLink } from "react-router-dom";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import Register from './component/Register_Login/RegisterPage';
 import Login from './component/Register_Login/LoginPage';
 import FindJob from './component/Job/FindJob';
@@ -14,23 +14,24 @@ import WorkProfile from './component/CV_Profile/UserInfo/WorkProfile';
 
 import CV_Create from './component/CV_Create';
 import JobDetails from './component/Job/JobDetails';
-
+import Contact from './component/CV_Contact/Contact'
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
-          <Route path="/job/detail/" element={<JobDetails/>}/>
-          <Route path="/profile/setting/workprofile" element={<WorkProfile/>}/> 
-          <Route path="/profile/setting" element={<SettingProfile/>}/> 
-          <Route path="/profile" element={<Profile/>}/> 
-          <Route path="/register" element={<Register/>}/>  
-          <Route path="/login" element={<Login/>}/> 
-          <Route path="/job" element={<FindJob/>}/> 
-          <Route path="/cv_create" element={<CV_Create/>}/>
-          <Route path="/" element={<MainPage/>}/>  
-        </Routes>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/job/detail/" element={<JobDetails />} />
+        <Route path="/profile/setting/workprofile" element={<WorkProfile />} />
+        <Route path="/profile/setting" element={<SettingProfile />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/job" element={<FindJob />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cv_create" element={<CV_Create />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/component/CV_Profile/Profile.js
+++ src/component/CV_Profile/Profile.js
@@ -5,60 +5,64 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { storage } from '../../firebase';
 import { ref, uploadBytes } from "firebase/storage"
-import {v4 } from 'uuid'
+import { v4 } from 'uuid'
+
+
+
+
 
 function Profile(location) {
+
 
     const [selectedImage, setSelectedImage] = useState(null);
     console.log(selectedImage)
     const uploadImageToServer = () => {
-        if(selectedImage == null) return;
-        const imageRef = ref(storage,`images/${selectedImage.name + v4()}`);
-        uploadBytes(imageRef,selectedImage).then(() => {
+        if (selectedImage == null) return;
+        const imageRef = ref(storage, `images/${selectedImage.name + v4()}`);
+        uploadBytes(imageRef, selectedImage).then(() => {
             alert("Image Uploaded!");
         }
-        
-           
+
+
         )
     }
-    
-    const userLogin = useSelector((state) => state.userLogin );
-    const {userInfo, loading, error} = userLogin;
+
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo, loading, error } = userLogin;
     const dispatch = useDispatch();
     console.log(userInfo)
-     useEffect(() => {
-   if(!userInfo){
-    window.location = '/login';
-   }
-  },[userInfo])
- 
-     var button = document.querySelector('button')
-        if (button !== null) {
-            button.onclick = () => {
-                window.print();
-            }
-            function move() {
-                location.assign("index2.html")
-            }
+    useEffect(() => {
+        if (!userInfo) {
+            window.location = '/login';
         }
-    
+    }, [userInfo])
+
+    var button = document.querySelector('button')
+    if (button !== null) {
+        button.onclick = () => {
+            window.print();
+        }
+
+    }
+
 
     return (
         <div>
             <div class="print-area">
                 <div class="header bg-amber-600">
+
                     <form action="post" method='post'>
                         {selectedImage && (
-                            <> 
+                            <>
                                 <img src={URL.createObjectURL(selectedImage)} id="image" />
-                                <button onClick={()=>setSelectedImage(null)}>Remove</button>
-                                
+                                <button onClick={() => setSelectedImage(null)} >Remove</button>
+
                             </>
-                          
+
                         )
-                           
+
                         }
-                       
+
                         <input onChange={(event) => setSelectedImage(event.target.files[0])} type="file" name="" class="custom-file-input" id="imageFile"
                             accept="image/gif , image/jpeg , image/png" />
                         {/* <button onClick={uploadImageToServer}>Upload</button> */}
@@ -73,10 +77,10 @@ function Profile(location) {
                 <div class="content">
                     <div class="left-area">
                         <div class="contact">
-                            <h4>CONTACT</h4>
-                            <h5>Name</h5>
+                            <h4>Liên hệ</h4>
+                            <h5>Tên</h5>
                             <p>{userInfo?.name}</p>
-                            <h5>Phone</h5>
+                            <h5>Điện thoại </h5>
                             <p>{userInfo?.contactNumber}</p>
                             <h5>Email</h5>
                             <p>{userInfo?.email}</p>
@@ -88,16 +92,16 @@ function Profile(location) {
                                     userInfo?.skills?.map((skill) => (
                                         <>
                                             <div class="bar">
-                                               <p>{skill}</p>
-                                               <span></span>
-                                            </div>  
+                                                <p>{skill}</p>
+                                                <span></span>
+                                            </div>
                                         </>
                                     )
                                     )
-                                        
-                                    
+
+
                                 }
-                         
+
                             </div>
                         </div>
                         <div class="follow">
@@ -113,11 +117,11 @@ function Profile(location) {
                             <h1 /><span>
                                 <i class="far fa-user"></i>
                                 ABOUT ME</span>
-                          <p>{userInfo?.aboutMe}</p>
+                            <p>{userInfo?.aboutMe}</p>
                         </div>
                         <div class="work">
                             <h1><span><i class="fas fa-suitcase-rolling"></i>
-                                WORK EXPERIENCE</span></h1>
+                                Kinh nghiệm làm việc</span></h1>
                             <div class="work-group">
                                 <h3>LEAD WEB DESIGNER</h3>
                                 <h4>ETC COLLEGE AMERICA</h4>
@@ -193,7 +197,7 @@ function Profile(location) {
                         </div>
                         <div class="education">
                             <h1><span><i class="fas fa-book"></i>
-                                EDUCATION</span></h1>
+                                Học vấn</span></h1>
                             <div class="edu-group">
                                 <h4>Go to sleep <br />
                                 </h4>
@@ -222,34 +226,7 @@ function Profile(location) {
                                     trời
                                     cao và trong xanh trên kia. Em tin chắc rằng mình sẽ thực hiện được ước mơ đó.</p>
                             </div>
-                            <div class="edu-group">
-                                <h4>Go to sleep <br />
-                                </h4>
-                                <span>1992/1997</span>
-                                <p>Đỗ Minh Giang là tên mà ba mẹ đặt cho lúc em mới ra đời. Nhưng mọi người trong nhà vẫn
-                                    quen
-                                    gọi
-                                    em là Bé Còi. Cái tên đó xem ra rất hợp với thân hình nhỏ nhắn và nói đúng hơn là còi
-                                    cọc
-                                    của
-                                    em. Ăn rất khoẻ nhưng em nghịch cũng dữ nên mẹ bảo em không thể lớn được. Tuy là con gái
-                                    nhưng
-                                    em nghịch như tụi con trai. Mỗi buổi chiều đi học về em thường cùng tụi thằng Sơn, thằng
-                                    Phúc
-                                    xách chai đi đổ dế. Hôm nào chán tụi em lại rủ nhau đi đánh đáo, đánh khăng. Trong nhà
-                                    em
-                                    toàn
-                                    bi, ảnh, quay, không thể kiếm đâu ra một con búp bê hay một bộ đồ hàng cả. Ba thường vừa
-                                    cười
-                                    vừa trêu em “đáng lẽ Bé Còi nhà ta phải là con trai mới đúng”. Thành tích học tập của em
-                                    rất
-                                    tốt. Năm ngoái em còn được nhà trường cử đi thi học sinh giỏi môn Toán nữa đấy. Mơ ước
-                                    lớn
-                                    nhất
-                                    của em là được trở thành phi công, lái những chiếc máy bay thật lớn, thật to bay lên bầu
-                                    trời
-                                    cao và trong xanh trên kia. Em tin chắc rằng mình sẽ thực hiện được ước mơ đó.</p>
-                            </div>
+
                         </div>
                     </div>
                 </div>
